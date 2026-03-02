a41a4f6941307a90815c587f7ede0ed7e8fb84e3
Ashu Gupta <ashugt73@gmail.com>
2026-03-02 11:45:29 +0530

    Finished with the signup

---
+++ app1/user_service/signup/main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI,Depends,HTTPException
+from pydantic import BaseModel
+from database import engine,SessionLocal
+from models import Base,User
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_db():
+	db = SessionLocal()
+	try:
+		yield db
+	finally:
+		db.close()
+
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def home():
+	return{
+	"Signup for users working"
+}
+
+class Usercreate(BaseModel):
+	username: str
+	email : str
+	password : str
+
+@app.post("/signup")
+def signup(user:Usercreate, db: Session = Depends(get_db)):
+		hashed_password = pwd_context.hash(user.password)
+
+		new_user = User(
+		username = user.username,
+		email = user.email,
+		password = hashed_password
+		)
+
+		try:
+			db.add(new_user)
+			db.commit()
+			db.refresh(new_user)
+
+		except IntegrityError:
+			db.rollback()
+			raise HTTPException(
+				status_code=400,
+				detail="Email entered already Exists"
+				)
+		return {
+			"id":new_user.id,
+			"username":new_user.username,
+			"email":new_user.email
+			}
