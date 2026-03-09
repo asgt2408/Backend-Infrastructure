c513d1667f53cb3d48ddbb396255013bba7c5ddb
Ashu Gupta <ashugt73@gmail.com>
2026-03-09 22:50:26 +0530

    Issue with the /profile with /login

--- app1/user_service/login/main.py
+++ app1/user_service/login/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI,Depends
 from pydantic import BaseModel
-from models import User,Base
+from user_service.models import User,Base
 from sqlalchemy.orm import Session
-from database import engine,SessionLocal
+from user_service.database import engine,SessionLocal
 from passlib.context import CryptContext
 from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 
 app = FastAPI()
 
@@ -15,7 +15,6 @@ Base.metadata.create_all(bind=engine)
 
 SECRET_KEY="mysectetkey"
 ALGORITHM="HS256"
-
 
 def get_db():
 	db = SessionLocal()
@@ -33,23 +32,22 @@ def token(data : dict):
 	token = jwt.encode(data,SECRET_KEY,algorithm = ALGORITHM)
 	return token
 
-
 @app.post("/login")
-def login(user:Usercreate, db : Session = Depends(get_db)):
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
 
-	check_user = db.query(User).filter(User.email==user.email).first()
+    check_user = db.query(User).filter(User.email == form_data.username).first()
 
-	if not check_user:
-		return {"message": "Invalid Login"}
+    if not check_user:
+        return {"message": "Invalid Login"}
 
-	passw = pwd_context.verify(user.password[:72],check_user.password)
+    passw = pwd_context.verify(form_data.password[:72], check_user.password)
 
-	if not passw:
-		return {"message": "Password is Incorrect"}
+    if not passw:
+        return {"message": "Password is Incorrect"}
 
-	access_token = token({"email":check_user.email})
+    access_token = token({"email": check_user.email})
 
-	return {"Message": "Login Successfull",
-			"token" : access_token}
-
-
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
